83d058c15bcdc6d0ba58b81e944294c98d330d53
Ghostik-gh <feodor200@mail.ru>
2022-09-23 21:19:24 +0300

    compute area method

--- JavaLaba2/src/Point3d.java
+++ JavaLaba2/src/Point3d.java
@@ -3,7 +3,9 @@ public class Point3d {
     private double x;
     private double y;
     private double z;
-    private static double accuracy = 100d;
+
+    // Точность с которой хранятся числа
+    private static double precision = 1000d;
 
     public Point3d(double xInput, double yInput, double zInput) {
         x = xInput;
@@ -27,16 +29,6 @@ public class Point3d {
         return z;
     }
 
-    public void println() {
-        System.out.print(x + " ");
-        System.out.print(y + " ");
-        System.out.println(z);
-    }
-
-    public static void setPrecision(double accuracyInput) {
-        accuracy = accuracyInput;
-    }
-
     public void setX(double xInput) {
         x = xInput;
     }
@@ -49,6 +41,12 @@ public class Point3d {
         z = zInput;
     }
 
+    // Количество знаков после запятой
+    public static void setPrecision(double precisionInput) {
+        precision = precisionInput;
+    }
+
+    // Проверяет равенство точек возвращает true если равны иначе false
     public boolean equalsPoint3d(Point3d secondPoint) {
         if (secondPoint.x != this.x)
             return false;
@@ -59,16 +57,34 @@ public class Point3d {
         return true;
     }
 
+    // Вычисляет расстояние между двумя точками
     public static double distanceTo(Point3d firsPoint, Point3d secondPoint) {
-        return Math.round(
-                Math.sqrt(Math.pow((secondPoint.x - firsPoint.x), 2) +
-                        Math.pow((secondPoint.y - firsPoint.y), 2) +
-                        Math.pow((secondPoint.z - firsPoint.z), 2)) * accuracy)
-                / accuracy;
+        double x = Math.round(Math.sqrt(Math.pow((secondPoint.x - firsPoint.x), 2) +
+                Math.pow((secondPoint.y - firsPoint.y), 2) +
+                Math.pow((secondPoint.z - firsPoint.z), 2)) * precision) / precision;
+        return x;
+
     }
 
-    // public static double computeArea() {
+    // Проверка являются ли три точки различными
+    // ture если все точки различны
+    public static boolean isTriangle(Point3d firsPoint, Point3d secondPoint, Point3d thirdPoint) {
+        return (firsPoint.equalsPoint3d(secondPoint) || firsPoint.equalsPoint3d(thirdPoint)
+                || secondPoint.equalsPoint3d(thirdPoint)) ? false : true;
+    }
 
-    // return -1;
-    // }
+    // Вычисляет площадь для трех заданных точек
+    public static double computeArea(Point3d firsPoint, Point3d secondPoint, Point3d thirdPoint) {
+        double lineAB = distanceTo(firsPoint, secondPoint);
+        double lineBC = distanceTo(thirdPoint, secondPoint);
+        double lineAC = distanceTo(firsPoint, thirdPoint);
+        double perimeter = (lineAB + lineAC + lineBC) / 2;
+        double area = Math.round(
+                Math.sqrt(perimeter * (perimeter - lineAB) *
+                        (perimeter - lineAC) * (perimeter - lineBC))
+                        * precision)
+                / precision;
+
+        return isTriangle(firsPoint, secondPoint, thirdPoint) ? area : -1;
+    }
 }
