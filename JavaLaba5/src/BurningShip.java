c1140abc658e904fb3264e3149f45bd159c4b51b
Ghostik-gh <feodor200@mail.ru>
2022-09-29 00:47:00 +0300

    add 2 fractals

--- JavaLaba5/src/BurningShip.java
+++ JavaLaba5/src/BurningShip.java
@@ -1,22 +1,23 @@
 import java.awt.geom.Rectangle2D.Double;
 
-public class Tricorn extends FractalGenerator {
-    // Максимальное количество итераций
+public class BurningShip extends FractalGenerator {
     public static final int MAX_ITERATIONS = 2000;
 
     @Override
     public void getInitialRange(Double range) {
+
         range.x = -2;
-        range.y = -2;
-        range.height = 4;
-        range.width = 4;
+        range.y = -2.5;
+        range.height = 3;
+        range.width = 3;
+
     }
 
     @Override
     public int numIterations(double x, double y) {
-        // Функция для фрактала Tricorn
+        // Функция для фрактала BurningShip
         // Используется комплексное сопряжение
-        // Zn = (Zn-1)^2 + c
+        // zn= (|Re(zn-1)| + i|Im(zn-1)|)2+ c.
         // Ограничения для функции
         // |Z| > 2
         // или пока меньше MAX_ITERATIONS
@@ -24,12 +25,11 @@ public class Tricorn extends FractalGenerator {
         double zReal = 0.0; // Действительная
         double zComplex = 0.0; // и комплексная часть
         while (iteration < MAX_ITERATIONS && zReal * zReal + zComplex * zComplex < 4) {
-            // Z0 = 0, Z1 = Z0 * Z0 + c = x + y*i, Z2 = ...
             // Xn+1 = Xn^2 + Yn^2 + x; Yn+1 = 2*Xn*Yn + y
             // from wikipedia)
             double zRealNew = zReal * zReal - zComplex * zComplex + x;
             // -2 вместо 2
-            double zComplexNew = -2 * zReal * zComplex + y;
+            double zComplexNew = Math.abs(2 * zReal * zComplex) + y;
             zReal = zRealNew;
             zComplex = zComplexNew;
             iteration++;
