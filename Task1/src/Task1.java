589b506d391caf812d182f5edfa822ee0d7f462d
Ghostik-gh <feodor200@mail.ru>
2022-10-06 14:27:21 +0300

    examples

--- Task1/src/Task1.java
+++ Task1/src/Task1.java
@@ -1,6 +1,38 @@
 public class Task1 {
     public static void main(String[] args) throws Exception {
-        System.out.println(addUpTo(10));
+        System.out.println(remainder(1, 3)); // 1
+        System.out.println(remainder(3, 4)); // 3
+        System.out.println(remainder(-9, 45)); // -9
+        System.out.println(remainder(5, 5)); // 0
+        System.out.println(triArea(3, 2)); // 3
+        System.out.println(triArea(7, 4)); // 14
+        System.out.println(triArea(10, 10)); // 50
+        System.out.println(animals(2, 3, 5)); // 36
+        System.out.println(animals(1, 2, 3)); // 22
+        System.out.println(animals(5, 2, 8)); // 50
+        System.out.println(profitableGamble(0.2, 50, 9)); // true
+        System.out.println(profitableGamble(0.9, 1, 2)); // false
+        System.out.println(profitableGamble(0.9, 3, 2)); // true
+        System.out.println(operation(24, 15, 9)); // "added"
+        System.out.println(operation(24, 26, 2)); // "subtracted"
+        System.out.println(operation(15, 11, 11)); // "none"
+        System.out.println(ctoa('A')); // 65
+        System.out.println(ctoa('m')); // 109
+        System.out.println(ctoa('[')); // 91
+        System.out.println(ctoa('\\')); // 92
+        System.out.println(addUpTo(3)); // 6
+        System.out.println(addUpTo(10)); // 55
+        System.out.println(addUpTo(7)); // 28
+        System.out.println(nextEdge(8, 10)); // 17
+        System.out.println(nextEdge(5, 7)); // 11
+        System.out.println(nextEdge(9, 2)); // 10
+        System.out.println(sumOfCubes(new int[] { 1, 5, 9 })); // 855
+        System.out.println(sumOfCubes(new int[] { 3, 4, 5 })); // 216
+        System.out.println(sumOfCubes(new int[] { 2 })); // 8
+        System.out.println(sumOfCubes(new int[] {}));// 0
+        System.out.println(abcmath(42, 5, 10));// false
+        System.out.println(abcmath(5, 2, 1)); // true
+        System.out.println(abcmath(1, 2, 3)); // false
     }
 
     public static int remainder(int x, int y) {
@@ -12,7 +44,7 @@ public class Task1 {
     }
 
     public static int animals(int chikens, int cows, int pigs) {
-        return chikens * 2 + (cows + pigs) * 2;
+        return chikens * 2 + (cows + pigs) * 4;
     }
 
     public static boolean profitableGamble(double prob, double prize, double pay) {
